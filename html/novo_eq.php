7458808e447132894d760d490201fc6ed51c0c35
lucascomputacao <lucascomputacao@gmail.com>
2014-11-19 10:33:58 -0300

    verficar sessão nesta página

--- html/novo_eq.php
+++ html/novo_eq.php
@@ -1,4 +1,16 @@
-
+<?php 
+// Iniciando ou reassumindo sessão
+session_start(); 
+// confirmando sessão 
+if(!isset($_SESSION['usuario']) && (!isset($_SESSION['senha']))){   
+    header("Location: ../login.php");      
+}
+// destruir sesssão
+if(isset($_REQUEST['sair'])){   
+    session_destroy();
+    header("Location: ../login.php");   
+}
+?>
 <table align = "center" border="2"   cellpadding="0"  cellspacing="1" width=70%>
 
     <th align="left"><font size=4 >Usuario: xxxxxxxxx xxxxxxxxxx ---------- Novo equipamento</th>
@@ -15,17 +27,17 @@
                 <tr height=70%><td></td></tr>
                 <tr height=70%><td></td></tr>
                 <tr height=70%><td></td></tr>
-				<tr height=70%><td>GERENCIAR CHAVES</td></tr>
+                <tr height=70%><td>GERENCIAR CHAVES</td></tr>
                 <tr height=70%><td><a href ="../ocorrencia/ocorrenciaC.php?matricula=<?php echo $matricula?>">Emprestar</a></td></tr>
                 <tr height=70%><td><a href ="../historico/historicoC.php?matricula=<?php echo $matricula?>">Historico</a></td></tr> 
                 <tr height=70%><td><a href ="../relatorios/relatoriosC.php?matricula=<?php echo $matricula?>">Relatorios</a></td></tr>
-               	<tr height=70%><td>GERENCIAR CHAVES</td></tr>
+                <tr height=70%><td>GERENCIAR CHAVES</td></tr>
                 <tr height=70%><td><a href ="../ocorrencia/ocorrenciaC.php?matricula=<?php echo $matricula?>">Emprestar</a></td></tr>
                 <tr height=70%><td><a href ="../historico/historicoC.php?matricula=<?php echo $matricula?>">Historico</a></td></tr> 
                 <tr height=70%><td><a href ="../relatorios/relatoriosC.php?matricula=<?php echo $matricula?>">Relatorios</a></td></tr>
-				<tr height=70%><td><a href ="../relatorios/relatoriosC.php?matricula=<?php echo $matricula?>">Novo equipamento</a></td></tr>
+                <tr height=70%><td><a href ="../relatorios/relatoriosC.php?matricula=<?php echo $matricula?>">Novo equipamento</a></td></tr>
                 <tr height=70%><td><a href ="logoff.php">Sair</a></td></tr>
-              
+
 
 
             </table>
@@ -37,19 +49,19 @@
         <td   width="80%" >
 
             <table  align="LEFT"  cellpadding="0"  cellspacing="1"  >
-       <tr>	<td>Novo equipamento:<input type="text" name="Neq">  </td> 
-			<td>   </td>
-			<td>Codigo:<input type="text" name="codeq">  </td> 
-			<td>   </td>
-					
-			<td>Quantidade:<input type="text" name="qtdeq">  </td> 
-			
-			
-			</tr>
-			
-					
-<tr>
-<TD><input type="button"  value="Cadastrar"></TD></tr>		  
+             <tr>	<td>Novo equipamento:<input type="text" name="Neq">  </td> 
+                 <td>   </td>
+                 <td>Codigo:<input type="text" name="codeq">  </td> 
+                 <td>   </td>
+
+                 <td>Quantidade:<input type="text" name="qtdeq">  </td> 
+
+
+             </tr>
+
+
+             <tr>
+                <TD><input type="button"  value="Cadastrar"></TD></tr>		  
 
             </table>
 
