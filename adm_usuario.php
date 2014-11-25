f7037562ecca1e1520031cbcba71a0e58958f25a
lucascomputacao <lucascomputacao@gmail.com>
2014-11-25 11:08:32 -0300

    deleção lista de usuários

---
+++ adm_usuario.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: lucas
+ * Date: 25/11/14
+ * Time: 11:06
+ */
+
+require_once 'sm.php';
+require_once 'core/Usuario.php';
+
+$a = new Usuario();
+
+// delete das pessoas, as multiplas selecionadas na lista
+if(isset($_POST['del'])){
+    if(is_array($_POST['del'])){
+
+        foreach ($_POST['del'] as $id) {
+            $a->setId($id);
+            $a->Delete();
+        }
+    }
+}
+
+
+
+
+$sm->assign('lista',$a->Select());
+
+
+$sm->display("adm_usuarios.tpl");
+?>
