00f05ed62c613c7d805c5901ac9d658770908cec
lucascomputacao <lucascomputacao@gmail.com>
2014-11-25 11:05:32 -0300

    cadastro usuario

---
+++ adm_usuario_cad.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: lucas
+ * Date: 25/11/14
+ * Time: 10:59
+ */
+
+require_once 'sm.php';
+require_once 'core/Usuario.php';
+
+$a = new Usuario();
+
+if(isset($_POST['nome']) and isset($_POST['senha']) ){
+
+    if($_POST['nome']!= ''  and $_POST['senha']){
+
+        // deve cadastrar
+        $a->setNome($_POST['nome']);
+        $a->criptSenha($_POST['senha']);
+
+        $a->Insert();
+
+        $sm->assign("done",true);
+
+    }// fim do tem preenchido
+
+}// fim do isset
+
+$sm->display("adm_usuario_cad.tpl");
+?>
